8276bc8384f959402a936a6c531b9e7dd1a4888b
em_wilson <emilyellenwilson@gmail.com>
2021-11-12 13:46:59 -0500

    Added back button and search box

--- js/script.js
+++ js/script.js
@@ -16,6 +16,12 @@ const repoData = document.querySelector(".repo-data");
 //????CURIOUS HOW WE CAN NAME THIS REPO DATA WHEN WE HAVE A REPODATA VAR IN THE GITREPOS ASYNC FUNCTION
 //AWAITING GUIDANCE FROM SLACK
 
+//Select Back To Repo Button
+const backButton = document.querySelector(".view-repos"); 
+
+//Select input with Search by Name placeholder
+const filterInput = document.querySelector(".filter-repos");
+
 //async function fetching data from github API
 const gitUserInfo = async function () {
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -60,6 +66,7 @@ const gitRepos = async function () {
 //Display fetched info on repos
 //repos is the parameter so the function accepts the data returned from the last fetchRepo API
 const displayRepos = function (repos) {
+    filterInput.classList.remove("hide"); 
     //loop and create a list item for each repo
     for (const repo of repos) {
         const repoItem = document.createElement("li"); //create a list
@@ -95,13 +102,15 @@ const getRepoInfo = async function(repoName) {
     }
     // console.log(languages); 
     
-    displayRepoInfo(repoInfo, languages); 
+    displayRepoInfo(repoInfo, languages); //calls the displayRepoInfo function 
 }; 
 
+//Create function to display specific repo information
 const displayRepoInfo = function (repoInfo, languages) {
     repoData.innerHTML = ""; 
     repoData.classList.remove("hide"); //Unhide the repo data element
     allReposContainer.classList.add("hide"); //Hide the element with the class of repos
+    backButton.classList.remove("hide"); //show the back button when clicked
     const div = document.createElement("div"); //creating new div
     div.classList.add("repo-data"); 
     div.innerHTML = ` 
@@ -113,5 +122,30 @@ const displayRepoInfo = function (repoInfo, languages) {
     `;
     repoData.append(div); 
 }; 
+
+//Create event listener for when user clicks on back to repos button
+backButton.addEventListener("click", function () {
+    allReposContainer.classList.remove("hide"); //show or unhide the list of all repos
+    repoData.classList.add("hide"); //remove or hide the individual repo data
+    backButton.classList.add("hide"); //remove or hide the back button when clicked
+}); 
+
+//Dynamic Search
+//Create an "input" event listener for the filterInput field
+filterInput.addEventListener("input", function(e) { //pass the (e) in the callback function
+    const searchText = e.target.value; //capture the value of the search text
+    // console.log(searchText);
+    const repos = document.querySelectorAll(".repo"); //create var to select ALL elements with class of "repo"
+    const searchLowerText = searchText.toLowerCase(); //
+
+    for (const repo of repos) {//loop through each repo inside the repos element
+        const repoLowerText = repo.innerText.toLowerCase(); //assign var to lowercase value of the innerText of each repo
+        if (repoLowerText.includes(searchLowerText)) { //Check to see if lowercase repo includes lowercase text. 
+            repo.classList.remove("hide"); //If the repoLowerText includes lowercase search text, show the repo
+        } else {
+            repo.classList.add("hide"); //If it doesn't include lowercase search text, hide the repo. 
+        }
+    }
+});
 
 
